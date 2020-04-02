3bdb8638d181fc8f7b62c58d7ba73ebb5f775789
zuzanna <zpietruszynska@gmail.com>
2020-04-02 21:02:59 +0200

    unified methods  in permissions.py

--- modifipic_app/img_modifier/permissions.py
+++ modifipic_app/img_modifier/permissions.py
@@ -10,7 +10,7 @@ class GetPostOrAuthenticated(permissions.BasePermission):
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        if request.method == 'GET'or request.method == 'POST':
+        if request.method == 'POST' or request.method == 'GET':
             return True
 
         return request.user and request.user.is_authenticated
